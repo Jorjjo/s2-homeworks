ec635e9020ff16793628ec2904b310a8f3697add
Daria Mochalova <shichimiya74@gmail.com>
2026-09-09 14:17:53 +0500

    hw-10

---
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -0,0 +1,35 @@
+type LoadingStateType = {
+    isLoading: boolean;
+};
+
+const initState: LoadingStateType = {
+    isLoading: false,
+};
+
+export const loadingReducer = (
+    state = initState,
+    action: LoadingActionType,
+): LoadingStateType => {
+    // fix any
+    switch (action.type) {
+        case 'CHANGE_LOADING':
+            return {
+                ...state,
+                isLoading: action.isLoading,
+            };
+        // пишет студент  // need to fix
+
+        default:
+            return state;
+    }
+};
+
+type LoadingActionType = {
+    type: 'CHANGE_LOADING';
+    isLoading: boolean;
+};
+
+export const loadingAC = (isLoading: boolean): LoadingActionType => ({
+    type: 'CHANGE_LOADING',
+    isLoading,
+});
